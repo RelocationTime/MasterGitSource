93dec10f053543a9c8244966442542cca8a6e13b
peter <peter.liu@liao.com>
2016-11-23 16:43:28 +0800

    getWindow Container

--- app/src/main/java/chaoxin/im/mastergitsource/MainActivity.java
+++ app/src/main/java/chaoxin/im/mastergitsource/MainActivity.java
@@ -2,12 +2,18 @@ package chaoxin.im.mastergitsource;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Window container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        container = getWindow().getContainer();
+        container.getContainer();
     }
 }
